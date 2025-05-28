b3b8b90af9056f5b2dbd41637fb8ca0390a9c4b9
ArtCodeKarine <karine@artcode.studio>
2025-05-28 22:10:08 +0200

    🎉new: : initialize Angular project with core setup and configurations
    - Added package.json with dependencies and scripts for Angular, NgRx, and Jest.
    - Created proxy configuration for backend API.
    - Set up Jest testing environment with initial setup file.
    - Implemented main application component with routing and translation support.
    - Added SCSS styles for application layout and spinner.
    - Created application configuration for routing and state management.
    - Defined utility models for date handling and state status.
    - Implemented translation service for internationalization.
    - Added mock server setup for development.
    - Included environment configurations for production and development.
    - Created initial HTML structure and main entry point for the application.
    - Established custom styles and typography for the application.
    - Configured TypeScript settings for application and testing.
    - Added TSLint configuration for code quality enforcement.

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { RouterModule } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterModule, MatProgressSpinnerModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  private _translate = inject(TranslateService);
+
+  constructor() {
+    this._translate.setDefaultLang('fr');
+  }
+}
